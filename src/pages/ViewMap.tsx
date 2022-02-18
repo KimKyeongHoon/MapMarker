14c88072213cdacd0ac4ad8e9f998abcd525a45b
keonghoon.kim <keonghoon.kim@nts-corp.com>
2022-02-18 15:56:18 +0900

    사이드 탭 추가

--- src/pages/ViewMap.tsx
+++ src/pages/ViewMap.tsx
@@ -1,11 +1,26 @@
-import react from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
 import KakaoMap from "../components/KakaoMap";
 import style from "./ViewMap.scss";
 
 const ViewMap = () => {
+  const [isHideTab, toggleSideTab] = useState(false);
+  const handleSideTab = () => {
+    toggleSideTab((isHideTab) => !isHideTab);
+  };
   return (
     <div className={style.root}>
-      <div className={style.side_wrap} />
+      <div
+        className={classnames(style.side_wrap, { [style.is_hide]: isHideTab })}
+      >
+        <button
+          type="button"
+          className={style.side_toggle}
+          onClick={handleSideTab}
+        >
+          ss
+        </button>
+      </div>
       <div className={style.map_wrap}>
         <div className={style.map}>
           <KakaoMap />
