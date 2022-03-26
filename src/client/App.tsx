c7eebdbb0bdb1ce37af502f8aca7a6643d22a2ef
keonghoon.kim <keonghoon.kim@nts-corp.com>
2022-03-27 03:07:07 +0900

    Bottom 컴포넌트 및 탭 선택 별 렌더링 추가

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ViewMap from "./pages/ViewMap";
+import Header from "./components/common/Header";
+import Bottom, { TabType } from "./components/common/Bottom";
 import "client/scss/reset.scss";
 
 function App() {
-  return <ViewMap />;
+  const [nowMapType, setMapType] = useState(TabType.Map);
+
+  const handleMapType = (type: TabType) => {
+    setMapType(type);
+  };
+
+  const renderMap = (type: TabType) => {
+    switch (type) {
+      case TabType.Map:
+        return <ViewMap />;
+      case TabType.PictureMap:
+        return "사진지도";
+      case TabType.FriendList:
+        return "친구목록";
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      {/* [D] Render에서 min-width 설정 */}
+      <Header />
+      {renderMap(nowMapType)}
+      <Bottom onClickTab={handleMapType} />
+    </>
+  );
 }
 
 export default App;
